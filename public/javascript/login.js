1aa73a9ff93afbed28228dc563cb903661a8223b
Dave Quinn <dquinn8@cogeco.ca>
2021-11-24 11:29:08 -0500

    user can sign up

--- public/javascript/login.js
+++ public/javascript/login.js
@@ -19,7 +19,7 @@ async function signupFormHandler(event) {
 
         // check the response status
         if (response.ok) {
-            document.location.replace('/dashboard');
+            document.location.replace('/');
         } else {
             response.json()
                 .then(res => alert(res.message));
@@ -47,7 +47,7 @@ async function loginFormHandler(event) {
         });
 
         if (response.ok) {
-            document.location.replace('/dashboard');
+            document.location.replace('/');
         } else {
             response.json()
                 .then(res => alert(res.message));
