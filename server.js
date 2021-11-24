d4155f407de7dddb731acee77e36e13e377b639c
randy <baumer.randy@gmail.com>
2021-11-23 21:09:36 -0500

    added abolity to upload to /uploads

--- server.js
+++ server.js
@@ -2,37 +2,6 @@ const path = require("path");
 const express = require("express");
 const routes = require('./controllers');
 const sequelize = require("./config/connection");
-const path = require('path');
-
-// For uploading photos
-const multer = require("multer");
-const storage = multer.diskStorage({
-    destination: (req, file, cb) => {
-        db(err, 'uploads');
-    },
-    filename: (req, file, cb) => {
-        console.log(file);
-        cb(err, Date.now() + path.extname(file.originalname));
-    },
-});
-
-const upload = multer({ storage: 'storage' });
-
-// We need this in the handlebars
-// <form action="/profile" method="post" enctype="multipart/form-data">
-//   <input type="file" name="image" />
-//   <input type='submit' />
-// </form>
-
-app.get('/upload', (req, res) => {
-    res.render('anyHTML');
-});
-
-app.post('/upload', upload.single('image'), (req, res) => {
-    res.send('image uploaded');
-})
-
-//const helpers = require('./utils/helpers');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -42,7 +11,40 @@ const hbs = exphbs.create({}); //helpers });
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
+// For uploading photos
+const multer = require("multer");
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, 'uploads');
+    },
+    filename: (req, file, cb) => {
+        console.log(file);
+        cb(null, Date.now() + path.extname(file.originalname));
+    },
+});
+
+const upload = multer({storage: storage});
+
+// We need this in the handlebars
+/* <form action="/profile" method="post" enctype="multipart/form-data">
+  <input type="file" name="image" />
+  <input type='submit' />
+</form> */
+
+app.get('/', (req, res) => {
+    res.render('homepage.handlebars');
+});
+
+app.post('/', upload.single('image'), (req, res) => {
+    res.send('image uploaded');
+})
+
+//const helpers = require('./utils/helpers');
+
+
+
 const session = require("express-session");
+const { diskStorage } = require("multer");
 const SequelizeStore = require("connect-session-sequelize")(session.Store);
 const sess = {
   secret: process.env.DB_SECRET,
