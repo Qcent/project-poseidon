b20fcd0e4fddf851dfbf83f97f6e3a149f2c75fd
Dave Quinn <dquinn8@cogeco.ca>
2021-11-24 13:20:50 -0500

    added Category model

---
+++ models/Category.js
@@ -0,0 +1,40 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+// create our Category model
+class Category extends Model {}
+
+// define table columns and configuration
+Category.init({
+    // define an id column
+    id: {
+        // use the special Sequelize DataTypes object provide what type of data it is
+        type: DataTypes.INTEGER,
+        // this is the equivalent of SQL's `NOT NULL` option
+        allowNull: false,
+        // instruct that this is the Primary Key
+        primaryKey: true,
+        // turn on auto increment
+        autoIncrement: true
+    },
+    // define a name column
+    name: {
+        type: DataTypes.INTEGER,
+        allowNull: false
+    },
+}, {
+    // TABLE CONFIGURATION OPTIONS GO HERE (https://sequelize.org/v5/manual/models-definition.html#configuration))
+    // pass in our imported sequelize connection (the direct connection to our database)
+    sequelize,
+    // dont automatically create createdAt/updatedAt timestamp fields
+    timestamps: false,
+    // don't pluralize name of database table
+    freezeTableName: true,
+    // use underscores instead of camel-casing (i.e. `comment_text` and not `commentText`)
+    underscored: true,
+    // make it so our model name stays lowercase in the database
+    modelName: 'category'
+
+});
+
+module.exports = Category;
--- models/index.js
+++ models/index.js
@@ -1,6 +1,7 @@
 const User = require('./User');
 //const Post = require('./Post');
 const Message = require('./Message');
+const Category = require('./Category');
 
 /*
 
@@ -13,6 +14,10 @@ Post.belongsTo(User, {
     foreignKey: 'user_id',
 });
 
+Post.belongsTo(Category, {
+    foreignKey: 'category_id',
+});
+
 */
 
-module.exports = { User, Message };
+module.exports = { User, Message, Category };
