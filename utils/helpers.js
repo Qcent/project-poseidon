85530c2a21f88bf150d85eb439f08e02cd549fba
Dave Quinn <dquinn8@cogeco.ca>
2021-11-25 09:02:20 -0500

    fixed password placeholder to indecate 8 characters are required and added spaced to format_date helper function output

--- utils/helpers.js
+++ utils/helpers.js
@@ -9,8 +9,8 @@ module.exports = {
         const Difference_In_Hours = Difference_In_Time / (1000 * 3600);
         const Difference_In_Minutes = Difference_In_Time / (1000 * 60);
 
-        if (Difference_In_Hours < 1) return `${Difference_In_Minutes.toFixed(0)}min ago`;
-        if (Difference_In_Hours < 48) return `${Difference_In_Hours.toFixed(0)}hours ago`;
+        if (Difference_In_Hours < 1) return `${Difference_In_Minutes.toFixed(0)} min ago`;
+        if (Difference_In_Hours < 48) return `${Difference_In_Hours.toFixed(0)} hours ago`;
 
         return `${new Date(date).getMonth() + 1}/${new Date(date).getDate()}/${new Date(date).getFullYear()}`;
     },
