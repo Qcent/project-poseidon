9923e357e0bd33fcccab79f47fd51965dee4c7b5
randy <baumer.randy@gmail.com>
2021-11-25 19:43:20 -0500

    moving uploading image into its own post route

--- server.js
+++ server.js
@@ -12,36 +12,6 @@ const hbs = exphbs.create({}); //helpers });
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
-// For uploading photos
-const multer = require("multer");
-fs.mkdir('uploads', err => {
-    if(err) {
-        fs.unlink('upload', err => {
-            console.log(err);
-        })
-    }
-});
-
-const storage = multer.diskStorage({
-    destination: (req, file, cb) => {
-        cb(null, 'uploads');
-    },
-    filename: (req, file, cb) => {
-        console.log(file);
-        cb(null, Date.now() + path.extname(file.originalname));
-    },
-});
-
-const upload = multer({storage: storage});
-
-app.get('/', (req, res) => {
-    res.render('homepage.handlebars');
-});
-
-app.post('/', upload.single('image'), (req, res) => {
-    res.send('image uploaded');
-})
-
 const helpers = require('./utils/helpers');
 
 const session = require("express-session");
