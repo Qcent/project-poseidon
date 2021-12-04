30131441ea1ba13860725608153f27c9180d72f6
Dave Quinn <dquinn8@cogeco.ca>
2021-12-04 14:41:37 -0500

    added small screen category drop down and fixed error on server load

---
+++ public/javascript/sort-by-category.js
@@ -0,0 +1,32 @@
+const sortByCategoryHandler = async(event) => {
+        const searchString = document.querySelector('#mainSearchInput').value.trim();
+        let searchCategory = event.target.options[event.target.selectedIndex].text
+
+        if (searchCategory === 'All') searchCategory = '';
+        if (searchString) {
+            document.location.replace(`/${searchCategory}?search=${searchString}`);
+        } else {
+            document.location.replace(`/${searchCategory}`);
+        }
+    }
+    (whichCategory = () => {
+        let searchCategory = '';
+        if (document.querySelector('#searchCategory')) {
+            searchCategory = document.querySelector('#searchCategory').value.trim();
+            if (searchCategory === 'All') searchCategory = '';
+
+        } else { console.log("no category"); }
+        if (searchCategory) {
+            console.log("category: " + searchCategory);
+            let list = document.querySelector('#sort-post-category');
+            //console.log(list.options)
+            Array.from(list.options).forEach((sel, idx) => {
+                if (sel.text == searchCategory) {
+                    list.selectedIndex = idx;
+                    console.log(idx);
+                }
+            });
+        }
+    })();
+if (document.querySelector('#sort-post-category'))
+    document.querySelector('#sort-post-category').addEventListener('change', sortByCategoryHandler);
